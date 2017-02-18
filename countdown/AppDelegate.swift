22a35996fa7623f2025902bf5065b2cba0cfcdbb
Evdodima <evdodima@gmail.com>
2017-02-18 23:18:00 +0300

    transparent tab bar

--- countdown/AppDelegate.swift
+++ countdown/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let tabBar = UITabBar.appearance()
+        tabBar.barTintColor = UIColor.clear
+        tabBar.backgroundImage = UIImage()
+        tabBar.shadowImage = UIImage()
         // Override point for customization after application launch.
         return true
     }
